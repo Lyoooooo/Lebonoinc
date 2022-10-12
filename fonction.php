524a8a66b30d1ef09b0ce5becc4286a5f1b4e7f5
Lyoooooo <81254893+Lyoooooo@users.noreply.github.com>
2022-10-12 15:16:06 +0200

    ajouté la fonction connecte()
    connecte() return 0 si l'utilisateur est pas connecté et 1 si il l'est

--- fonction.php
+++ fonction.php
@@ -2,13 +2,16 @@
 
 function connexion()
 {
-    try {
-        $pdo = new PDO('mysql:dbname=bonu;host=127.0.0.1', 'root', ''); 
-    } catch (PDOException $e) {
-        $pdo = new PDO('mysql:dbname=bonu;host=127.0.0.1:3700', 'root', '');
-    }
+    $pdo = new PDO('mysql:dbname=bonu;host=127.0.0.1', 'root', '');
     return $pdo;
 }
 
+function connecte()
+{
+    if(!isset($_SESSION["idu"]))
+    {
+        return(0);
+    }else return(1);
+}
 
 ?>
