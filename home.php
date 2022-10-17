63b761f8193af6488ccbfa57c3e4da07f846d3ea
Guewaud <103487430+Guewaud@users.noreply.github.com>
2022-10-17 16:53:07 +0200

    sauv1
    Commencement de la nav bar

--- home.php
+++ home.php
@@ -1,16 +1,37 @@
 <!DOCTYPE html>
-<html lang="fr">
-
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="image/Bonumanguli3.png" />
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="bonum.css">
 </head>
-
 <body>
 
-</body>
+  <div class="head sticky-top">
+    <nav class="navbar navbar-toggler navbar-light bg-info">
+      <div class="container-fluid">
 
+      <img src="image/Bonumanguli3.png" width="70">
+
+      <a class="navbar-brand" href="home.php">Bonum Anguli</a>
+
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+          <div>
+              <img src="image/0.png" width="50">Mon compte
+          </div>
+          
+        </div>
+      </div>
+    </nav>
+  </div>
+
+    
+</body>
 </html>
