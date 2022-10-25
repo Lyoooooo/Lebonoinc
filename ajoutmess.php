876e7b175915072ebcf52798b2df11a26f0fe003
Lyoooooo <81254893+Lyoooooo@users.noreply.github.com>
2022-10-25 21:41:37 +0200

    fix recherche et chat

--- ajoutmess.php
+++ ajoutmess.php
@@ -3,13 +3,18 @@ include "fonction.php";
 session_start();
 $pdo = connexion();
 $idp = $_GET["idp"];
+$idsend = $_GET["idsend"];
 $idu = $_SESSION["idu"];
-$stmt = $pdo->prepare("SELECT * FROM produit WHERE idp=?");
-$stmt->execute([$idp]);
-$ligne = $stmt->fetch();
-$idrec = $ligne["idu"];
+if($idp == 0){
+    $idrec = $idsend;
+}else{
+    $stmt = $pdo->prepare("SELECT * FROM produit WHERE idp=?");
+    $stmt->execute([$idp]);
+    $ligne = $stmt->fetch();
+    $idrec = $ligne["idu"];
+}
 $mess = $_POST["mess"];
 $stmt = $pdo->prepare("INSERT INTO messages VALUES (?,?,?,?,?)");
 $stmt->execute([NULL,$idu,$idrec,$mess,date("Y-m-d H:i:s")]);
-header("refresh:0;url=chat.php?idp=$idp");
+header("refresh:0;url=chat.php?idp=$idp&idsend=$idsend");
 ?>
