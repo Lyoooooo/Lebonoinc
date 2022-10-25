94cc028f7ca7f9ceb1d39797b2841a6c1003b767
nqhtqn <97612424+nqhtqn@users.noreply.github.com>
2022-10-25 17:36:55 +0200

    CA AVANCE

--- fonction.php
+++ fonction.php
@@ -88,9 +88,9 @@ function headermain()
             <a href="home.php" id="textthird">
               <h2>Bonumanguli</h2>
             </a>
-            <form action="recherche.php" class="d-flex">
-              <input class="form-control me-2" type="search" placeholder="Rechercher">
-              <button class="btn btn-outline-success" type="submit">Rechercher</button>
+            <form action="recherche.php" method="get" class="d-flex">
+              <input class="form-control me-2" type="search" name="rec" placeholder="Rechercher">
+              <button class="btn btn-outline-success" name="bt" type="submit">Rechercher</button>
             </form>
           </div>
           <div class="col-3 text-center">
--- recherche.php
+++ recherche.php
@@ -1,2 +1,72 @@
-t'as cru que c'était fait et bah non haha
-tu va croire que j'ai avancé car j ai modifié la page mais tjr pas :)
+<?php
+include "fonction.php";
+session_start();
+$pdo = connexion();
+headsimple();
+$rec = $_GET["rec"];
+$bt =$_GET["bt"];
+if(isset($bt) && !empty(trim($rec))) {
+  $words=explode(" ",trim($rec));
+  for ($i=0;$i<count($words);$i++) {
+    $mot[$i] = "nomp like '%".$words[$i]."%'"; 
+  $res=$pdo->prepare("SELECT * FROM produit WHERE ".implode(" OR ", $mot));
+  $res->setFetchMode(PDO::FETCH_ASSOC);
+  $res->execute();
+  $tab=$res->fetchAll();
+  $afficher="oui";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="image/Bonumanguli5.png" />
+  <link rel="stylesheet" href="bonum.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <title>Bonumanguli - Recherche</title>
+</head>
+
+<body>
+  <form class="d-flex" method="get" role="search">
+    <input class="form-control me-2" name="rec" type="search" value="<?php echo $rec ?>" placeholder="Recherchez l'article de vos rêves" aria-label="Search">
+    <button class="btn btn-outline-success" name="bt" type="submit">Rechercher</button>
+  </form>
+
+  <?php if (@$afficher == "oui") { ?>
+  <div class="resultat">
+    <div class="nbr"><?=count($tab). " ".(count($tab)>1?"résultats trouvés":"résultat trouvé") ?></div>
+      
+    <div class="container" style="width: 80%;">
+      <div class="row">
+        <div class="col-3">
+          <div id="annonce">
+            <div class="card">
+              <img src="<?php echo $prod1["photo1"] ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title"><?php echo $prod1["nomp"] ?></h5><br>
+                  <h5 class="card-title"><?php echo $prod1["prix"] ?>€</h5><br>
+                  <a href="detailprod.php?idp=<?php echo $prod1["idp"] ?>" class="btn btn-primary">
+                  <img src="image/voir.png" width="20">Voir l'annonce</a>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div> 
+    
+    <?php for ($i=0;$i<count($tab);$i++) { ?>
+
+      <?php echo $tab[$i]["nomp"] ?>
+      <?php } ?>
+    
+  </div>
+  <?php } else {
+    echo "Non";
+  } ?>
+</body>
